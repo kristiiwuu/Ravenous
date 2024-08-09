a9b3eaeb772458dac9c9038d3803d293b7c50f1e
kristiiwuu <143127170+kristiiwuu@users.noreply.github.com>
2024-08-09 16:13:15 -0700

    Business Component

---
+++ src/components/Business/Business.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Business() {
+    return (
+        <div id='Business-parent'>
+            <div id='Business-img'>
+                <img src='https://www.craftycookbook.com/wp-content/uploads/2024/04/nigiri-sushi-1200.jpg' alt='sushi'></img>
+            </div>
+            <div id='Business-info'>
+                <div id='Business-address'>
+                    <p>1010 Paddington Way</p>
+                    <p>Bordertown</p>
+                    <p>NY 10101</p>
+                </div>
+                <div id='Business-misc'>
+                    <p>Italian</p>
+                    <p>4.5 stars</p>
+                    <p>90 reviews</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Business;
