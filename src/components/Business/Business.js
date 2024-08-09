47b4ec767b8de56464b8da7b69d16626a5c51875
kristiiwuu <143127170+kristiiwuu@users.noreply.github.com>
2024-08-09 16:43:17 -0700

    Business Component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,18 +1,17 @@
 import React from 'react';
-
+import './Business.css';
 function Business() {
     return (
-        <div id='Business-parent'>
-            <div id='Business-img'>
-                <img src='https://www.craftycookbook.com/wp-content/uploads/2024/04/nigiri-sushi-1200.jpg' alt='sushi'></img>
-            </div>
-            <div id='Business-info'>
-                <div id='Business-address'>
+        <div className='Business-parent'>
+            <img className='Business-img' src='https://www.craftycookbook.com/wp-content/uploads/2024/04/nigiri-sushi-1200.jpg' alt='sushi'></img>
+            <h3 className='Business-name'>Restaurant Name</h3>
+            <div className='Business-info'>
+                <div className='Business-address'>
                     <p>1010 Paddington Way</p>
                     <p>Bordertown</p>
                     <p>NY 10101</p>
                 </div>
-                <div id='Business-misc'>
+                <div className='Business-misc'>
                     <p>Italian</p>
                     <p>4.5 stars</p>
                     <p>90 reviews</p>
