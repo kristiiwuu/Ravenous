bbda4f960d8048542391abc9ebdd89ce9062e4ff
Kristi Wu <kawu@usc.edu>
2024-08-27 17:38:35 -0700

    refined SearchBar
    compared with Codecademy's solution

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function App() {
       <div className="App-business-row">
         <BusinessList />
       </div>
+      <div className="App-business-row">
+        <BusinessList />
+      </div>
       
   </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import './SearchBar.css';
 
-function SearchBar() {
-    const sort_by = {
+function SearchBar() { // alternative: const SearchBar = () => 
+    const sort_by = { // this can be moved outside function
         'Best Match': 'best_match',
         'Highest Rated': 'rating',
         'Most Reviewed': 'review_count'
     };
 
+    // Answer Key: created function 'const renderSortByOptions = () =>
+    // Object.keys(sort_by) --> returns the object's keys
+    // .map((sortByOption) => { let sortByOptionValue = sortByOptions[sortByOption]; return <li>...</li>}) --> maps through the keys and accesses value
+    const keys = ['Best Match', 'Highest Rated', 'Most Reviewed'];
+
+    const listItems = keys.map( key => <li key={sort_by[key]}>{key}</li>);
 
     return (
         <div className='SearchBar-parent'>
             <div className='SearchBar-sortby'>
-                
+                <ul>{listItems}</ul> 
+                {/* instead of listItems, calls renderSortByOptions() here*/}
             </div>
             <div className='SearchBar-input'>
                 <form>
-                    <input placeholder='Search Business'></input>
+                    <input placeholder='Search Businesses'></input>
                     <input placeholder='Where?'></input>
                 </form>
+            </div>
+            <div>
+                <button className='SearchBar-button'>Let's Go</button>
             </div>
         </div>
     )
